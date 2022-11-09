3f5ccc781bb5961ff0d13bccc8bac1da245b699d
Karley Thurston <84428087+kthurst1@users.noreply.github.com>
2022-11-09 09:46:50 -0600

    added MoodHelp Page

---
+++ src/views/MoodHelpView.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Mood Help</h1>
+    <p>
+    
+    </p>
+  
+     
+    <router-link to="/mood" class="back">
+      Back
+    </router-link>
+      
+  
+  </template>
+      
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+    .back {
+      background-color: rgb(10, 97, 190);
+      color: white;
+      text-decoration: none;
+      padding: 8px 12px;
+      border-radius: 3px;
+      transition: opacity 0.15s;
+    }
+    
+    .back:hover {
+      opacity: 0.8;
+    }
+  </style>
