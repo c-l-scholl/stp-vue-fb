f9eb437271a22b6775e50aecd258bdb04bbd880c
Karley Thurston <84428087+kthurst1@users.noreply.github.com>
2022-11-15 15:03:48 -0600

    updated start buttons - changed graphics and flow type

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,20 @@
 <template>
   <h1>heartBeat</h1>
-    <p>
-      Welcome to your personalized playlist generator, courtesy of Spotify music
-      and data. <br /> <br>
-      In a matter of
-      minutes, with your input heartbeat and mood, <br />
-      we will provide you a playlist of music to match your current state!
+  <p>
+    Welcome to your personalized playlist generator, courtesy of Spotify music
+    and data. <br /> <br>
+    In a matter of
+    minutes, with your input heartbeat and mood, <br />
+    we will provide you a playlist of music to match your current state!
     </p>
 
-    <button type="button">
-      <router-link to="/calc-bpm">
-        Start
-      </router-link>
-      
-    </button>
+    <router-link to="/calc-bpm" class="begin">
+      Start
+    </router-link>
 
-    <button type="button">
-      <router-link to="/about-main">
+    <router-link to="/about-main" class="about">
       About
     </router-link>
-    </button>
-
 </template>
     
 <script>
@@ -30,5 +24,32 @@ export default {
 </script>
 
 <style>
+  .begin,
+  .about {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 15px;
+    border-width: 0;
+    margin: 5px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    color: black;
+    transition: opacity 0.15s;
+    text-decoration: none;
+  }
 
+  .about:hover,
+  .begin:hover {
+    opacity: 0.6;
+  }
+
+  .begin {
+    font-size: 50px;
+    font-weight: bolder;
+  }
+  .about {
+    font-size:x-large;
+    font-weight: bold;
+  }
 </style>
