12db0700fc69688b1163412260b3e25b6faf9a1c
c-l-scholl <cscholl@macalester.edu>
2022-11-21 09:48:25 -0600

    adjusted pages to match heights of others

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,31 +1,38 @@
 <template>
+  <div class="page">
     <h1>About our Playlist Generator</h1>
-      <p>
-        Our playlist generator provides you with a list of songs perfect for <br>
-        your mood and feelings right now! Using just your heartbeat and <br>
-        current mood, we instantaneously offer you music to match. <br /> <br>
+    <p>
+      Our playlist generator provides you with a list of songs perfect for <br>
+      your mood and feelings right now! Using just your heartbeat and <br>
+      current mood, we instantaneously offer you music to match. <br /> <br>
 
-        Want your music to change your feelings? <br> <br>
+      Want your music to change your feelings? <br> <br>
 
-        No problem! We can help. <br>
-        Then just select the mood you want to be in!
+      No problem! We can help. <br>
+      Then just select the mood you want to be in!
 
 
 
     </p>
-  
+
     <router-link to="/" class="back">
       Back
     </router-link>
-  
-  </template>
+  </div>
+
+
+</template>
       
-  <script>
-  export default {
+<script>
+export default {
+
+}
+</script>
   
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+<style scoped>
+.page {
+  position: relative;
+  top: 50px;
+}
+
+</style>
