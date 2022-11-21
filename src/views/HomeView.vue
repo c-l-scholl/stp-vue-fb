4bd7a908bb69e1a217e6e4704bf73ef2e00fcbe6
c-l-scholl <cscholl@macalester.edu>
2022-11-21 09:41:52 -0600

    moved home page down

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <template>
-  <h1>heartBeat</h1>
-  <p>
-    Welcome to your personalized playlist generator, courtesy of Spotify music
-    and data. <br /> <br>
-    In a matter of
-    minutes, with your input heartbeat and mood, <br />
-    we will provide you a playlist of music to match your current state!
+  <div class="page">
+    <h1>heartBeat</h1>
+    <p>
+      Welcome to your personalized playlist generator, courtesy of Spotify music
+      and data. <br /> <br>
+      In a matter of
+      minutes, with your input heartbeat and mood, <br />
+      we will provide you a playlist of music to match your current state!
     </p>
 
     <router-link to="/calc-bpm" class="begin">
@@ -15,6 +16,8 @@
     <router-link to="/about-main" class="about">
       About
     </router-link>
+  </div>
+
 </template>
     
 <script>
@@ -23,33 +26,39 @@ export default {
 }
 </script>
 
-<style>
-  .begin,
-  .about {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px 15px;
-    border-width: 0;
-    margin: 5px;
-    padding: 10px 15px;
-    border-radius: 5px;
-    color: black;
-    transition: opacity 0.15s;
-    text-decoration: none;
-  }
+<style scoped>
+.page {
+  position: relative;
+  top: 50px;
+}
 
-  .about:hover,
-  .begin:hover {
-    opacity: 0.6;
-  }
+.begin,
+.about {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 15px;
+  border-width: 0;
+  margin: 5px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  color: black;
+  transition: opacity 0.15s;
+  text-decoration: none;
+}
 
-  .begin {
-    font-size: 50px;
-    font-weight: bolder;
-  }
-  .about {
-    font-size:x-large;
-    font-weight: bold;
-  }
+.about:hover,
+.begin:hover {
+  opacity: 0.6;
+}
+
+.begin {
+  font-size: 50px;
+  font-weight: bolder;
+}
+
+.about {
+  font-size: x-large;
+  font-weight: bold;
+}
 </style>
