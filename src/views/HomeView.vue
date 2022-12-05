6c36e0ddaf076c0698ff7356393b18874496a560
c-l-scholl <cscholl@macalester.edu>
2022-12-05 09:56:50 -0600

    home buttons no longer extend infinitely

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,12 +8,19 @@
       minutes, with your input heartbeat and mood, <br />
       we will provide you a playlist of music to match your current state!
     </p>
-    <router-link to="/calc-bpm" class="begin">
-      Start
-    </router-link>
-    <router-link to="/about-main" class="about">
-      About
-    </router-link>
+    <div class="routers">
+      <div class="begin-button">
+        <router-link to="/calc-bpm" class="begin">
+          Start
+        </router-link>
+      </div>
+      <div class="about-button">
+        <router-link to="/about-main" class="about">
+          About
+        </router-link>  
+      </div>
+    </div>
+    
   </div>
 </template>
   
@@ -21,12 +28,16 @@
 .page {
   position: relative;
   top: 50px;
+  
+}
+
+.begin-button,
+.about-button {
+  display: block;
+  margin: 30px;
 }
 .begin,
 .about {
-  display: flex;
-  justify-content: center;
-  align-items: center;
   padding: 10px 15px;
   border-width: 0;
   margin: 5px;
@@ -43,9 +54,11 @@
 .begin {
   font-size: 50px;
   font-weight: bolder;
+  max-width: fit-content;
 }
 .about {
   font-size: x-large;
   font-weight: bold;
+  max-width: fit-content;
 }
 </style>
