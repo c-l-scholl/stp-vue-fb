a07f45c814bf903151efc707cd00493cabcd8666
Karley Thurston <84428087+kthurst1@users.noreply.github.com>
2022-12-06 14:42:24 -0600

    added back button icon for all back buttons

--- src/components/BackButtonComp.vue
+++ src/components/BackButtonComp.vue
@@ -1,19 +1,22 @@
 <template>
   <button @click="$router.back()" class='go-back'>
-    Back
+    <img src="../images/icons8-back-arrow-30.png">
   </button>
+  <!-- Back icon by Icons8: https://icons8.com -->
 </template>
 
 <style scoped>
   .go-back {
     border-width: 0;
     margin: 5px;
-    padding: 10px 15px;
+    padding: 10px;
     border-radius: 5px;
     color: white;
     transition: opacity 0.15s;
     text-decoration: none;
     background-color: rgb(10, 97, 190);
+    max-width: 50px;
+    border-radius: 30px;
   }
   .go-back:hover {
     opacity: 0.8;
